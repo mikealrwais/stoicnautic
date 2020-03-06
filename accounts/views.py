1c99775545e24768ecc3998695aca6f0981659b9
Mike Al Rwais <mike.h.alrwais@me.com>
2020-03-05 19:12:34 -0800

    Create User Form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dailyjournal:list')
+    else:
+        context = {
+            'form' : UserCreationForm(),        
+            }
+    return render(request, 'accounts/signup.html', context)
