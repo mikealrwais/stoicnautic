4871be93b2061b6c7568316e0c0de1ba98e6f09f
Mike Al Rwais <mike.h.alrwais@me.com>
2020-03-07 09:14:20 -0800

    Verify User

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('dailyjournal:list')
     else:
         form = UserCreationForm()       
@@ -15,7 +17,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            
+            user = form.get_user()
+            login(request, user)
             return redirect('dailyjournal:list')  
     else:
         form = AuthenticationForm()
